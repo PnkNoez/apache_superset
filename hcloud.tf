43dc983c8929b10eab2ccc1fedfa4e8e2677b820
Nicolas <nr@formitas.de>
2023-12-22 15:06:50 +0100

    base setup hcloud with firewall and ssh

--- hcloud.tf
+++ hcloud.tf
@@ -27,17 +27,72 @@ resource "hcloud_server" "web" {
   name        = "qbd-apache-superset"
   image       = var.os_type
   server_type = var.server_type
+  firewall_ids = [hcloud_firewall.myfirewall.id, hcloud_firewall.default-egress.id]
+
+}
+
+resource "hcloud_ssh_key" "primary-ssh-key" {
+  name = "primary-ssh-key"
+  public_key = tls_private_key.generic-ssh-key.public_key_openssh
 }
 
 resource "tls_private_key" "generic-ssh-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
+
+  provisioner "local-exec" {
+    command = <<EOF
+    echo "${tls_private_key.generic-ssh-key.private_key_openssh} >> ~/src/apache/.envrc"
+    EOF
+  }
 }
 
-#resource "hcloud_volume" "storage" {
-#  name       = "my-volume"
-#  size       = 50
-#  server_id  = "${hcloud_server.web.id}"
-#  automount  = true
-#  format     = "ext4"
-#}
+
+resource "hcloud_firewall" "myfirewall" {
+  name = "my-firewall"
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction       = "in"
+    port            = "443"
+    protocol        = "tcp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+
+}
+
+resource "hcloud_firewall" "default-egress" {
+  labels = {}
+  name   = "default-egress"
+
+  rule {
+    direction       = "out"
+    port            = "443"
+    protocol        = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+
+  rule {
+    direction       = "out"
+    port            = "80"
+    protocol        = "tcp"
+    destination_ips = [
+      "0.0.0.0/0",
+      "::/0",
+    ]
+  }
+}
